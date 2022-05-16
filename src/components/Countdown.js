6849390c73b3126aa7e8a14a87e6d7f1b05f947e
khallslearn <75588413+khallslearn@users.noreply.github.com>
2022-05-15 21:19:39 -0400

    countdown

---
+++ src/components/Countdown.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import { useState } from 'react'
+
+
+const Countdown = () => {
+    const [counter, setCounter] = useState(0)
+
+
+  return (
+    <div className='App'>
+    <button onClick={() => setCounter((prevCount) => prevCount + 1)}>+</button>
+    <h1>{counter}</h1>
+    <button onClick={() => setCounter((prevCount) => prevCount - 1)} >-</button>
+    
+    </div>
+  )
+}
+
+export default Countdown
